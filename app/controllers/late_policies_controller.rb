aa20dc1e73a5f97e93f4e9eec75c3cf36100f650
Shawn Salekin <ssalek14@earlham.edu>
2022-03-20 19:15:18 -0400

    Fixed a bug: the consolidated input validation code was checking for duplicate name when the name was not changed during edit

--- app/controllers/late_policies_controller.rb
+++ app/controllers/late_policies_controller.rb
@@ -49,10 +49,10 @@ class LatePoliciesController < ApplicationController
       @late_policy = LatePolicy.new(late_policy_params)
       @late_policy.instructor_id = instructor_id
       begin
-        @penalty_policy.save!
+        @late_policy.save!
         flash[:notice] = 'The late policy was successfully created.'
         redirect_to action: 'index'
-      rescue StandardError
+      rescue StandrdError 
         flash[:error] = 'The following error occurred while saving the late policy: '
         redirect_to action: 'new'
       end
@@ -69,7 +69,7 @@ class LatePoliciesController < ApplicationController
       # message. TODO: need to check this is not breaking any tests.
     penalty_policy = LatePolicy.find(params[:id])
 
-    valid_penalty, error_message = validate_input
+    valid_penalty, error_message = validate_input(is_update=true)
     if error_message
       flash[:error] = error_message
       redirect_to action: 'edit', id: params[:id]
@@ -112,24 +112,38 @@ class LatePoliciesController < ApplicationController
     @penalty_policy ||= @late_policy || LatePolicy.find(params[:id]) if params[:id]
   end
 
-  def validate_input 
+  def duplicate_name_check(is_update=false)
+    should_check = true
+    if is_update
+        existing_late_policy = LatePolicy.find(params[:id])
+        if existing_late_policy.policy_name == params[:late_policy][:policy_name]
+            should_check = false
+        end
+    end
+
+    if should_check 
+      if LatePolicy.check_policy_with_same_name(params[:late_policy][:policy_name], instructor_id)
+          error_message = 'A policy with the same name `' + params[:late_policy][:policy_name] + '` already exists.'
+          valid_penalty = false
+      end
+    end
+    return valid_penalty, error_message
+  end
+
+
+  def validate_input(is_update=false)
     # Validates input for create and update forms
-
-    valid_penalty = true
-    error_message = nil
-
     max_penalty = params[:late_policy][:max_penalty].to_i
-
     invalid_penalty_per_unit = max_penalty < params[:late_policy][:penalty_per_unit].to_i
+
+    valid_penalty, error_message = true, nil
+    valid_penalty, error_message = duplicate_name_check(is_update)
+
     if invalid_penalty_per_unit
       error_message = 'The maximum penalty cannot be less than penalty per unit.'
       valid_penalty = false
     end
 
-    if LatePolicy.check_policy_with_same_name(params[:late_policy][:policy_name], instructor_id)
-      error_message = 'A policy with the same name already exists.'
-      valid_penalty = false
-    end
 
     if max_penalty >= 100
       error_message = 'Maximum penalty cannot be greater than or equal to 100'
